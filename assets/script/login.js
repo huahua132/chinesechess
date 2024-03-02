a191903d2a68cdadeda6b2e9c2ae3b7afe7eec23
huahua132 <958677003@qq.com>
2024-03-02 16:58:36 +0800

    对接登录流程

--- assets/script/login.js
+++ assets/script/login.js
@@ -4,6 +4,7 @@
 //  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+var global = require("global")
 
 cc.Class({
     extends: cc.Component,
@@ -62,6 +63,49 @@ cc.Class({
         let account_editBox = this.account.getComponent(cc.EditBox)
         let password_editBox = this.password.getComponent(cc.EditBox)
         console.log("onCommitClick >>> ", account_editBox.string, password_editBox.string)
+
+        let host = "http://127.0.0.1:11014"
+        let xhr = new XMLHttpRequest()
+        let data = {}
+        let code = 0
+        let message = ""
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+                var response = xhr.responseText;
+                response = JSON.parse(response)
+                console.log("response: ", response);
+                code = response.code
+                message = response.message
+                data = response.data
+            }
+        }
+
+        let url = ""
+        if (this.mtype == "login") {
+            url = host + '/user/login'
+        } else {
+            url = host + '/user/signup'
+        }
+        
+        xhr.open("POST", url, false)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(JSON.stringify({
+            account : account_editBox.string,
+            password : password_editBox.string
+        }));
+
+        //成功
+        if (code == 0) {
+            if (this.mtype == "login") {
+                //进入大厅
+                global.hallhost = data.host
+                global.halltoken = data.token
+                cc.director.loadScene("hall");
+            }
+        } else {
+            //失败
+
+        }
     },
     // update (dt) {},
 });
